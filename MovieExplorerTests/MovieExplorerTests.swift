d90d913ea14b20c111889e95c70c834bbc150f8d
Kirlos Yousef <kirlosek@gmail.com>
2025-07-09 18:00:12 +0300

    Initial Commit

---
+++ MovieExplorerTests/MovieExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerTests.swift
+//  MovieExplorerTests
+//
+//  Created by Kirlos Yousef on 09/07/2025.
+//
+
+import Testing
+@testable import MovieExplorer
+
+struct MovieExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
